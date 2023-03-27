b307c23c586bb7e0c8e1653b65c6c636a6c15be9
José Pedro Sousa <github@zepedro.me>
2023-03-27 21:33:45 +0100

    moving away from child process

--- scripts/genContract.ts
+++ scripts/genContract.ts
@@ -1,14 +1,45 @@
-import { execSync } from "child_process"
-import {existsSync, rmdirSync} from "fs"
-import path from "path"
+import { existsSync, readFileSync, writeFileSync, unlinkSync } from 'fs';
+import path from 'path';
+// @ts-ignore
+import { initialiseResolver } from '@noir-lang/noir-source-resolver';
+import { acir_read_bytes, compile } from '@noir-lang/noir_wasm';
+// @ts-ignore
+import { setup_generic_prover_and_verifier } from '@noir-lang/barretenberg';
+
+const MAIN_NR_PATH = 'src/main.nr';
 
 async function main() {
-  // if (existsSync(path.join(__dirname, "../contract/plonk_vk.sol"))) {
-  //   rmdirSync(path.join(__dirname, "../contract/plonk_vk.sol"), { recursive: true })
-  // }
+  if (existsSync(path.join(__dirname, '../contract/plonk_vk.sol'))) {
+    unlinkSync(path.join(__dirname, '../contract/plonk_vk.sol'));
+  }
 
-  execSync("nargo contract")
+  initialiseResolver(() => {
+    try {
+      const string = readFileSync(MAIN_NR_PATH, { encoding: 'utf8' });
+      return string;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  });
 
+  console.log('Compiling...');
+  const compiled = await compile({});
+
+  const acir_bytes = new Uint8Array(Buffer.from(compiled.circuit, 'hex'));
+  const acir = acir_read_bytes(acir_bytes);
+
+  const [prover, verifier] = await setup_generic_prover_and_verifier(acir);
+  console.log('Generating contract...');
+
+  const sc = verifier.SmartContract();
+
+  console.log('Contract generated!');
+  writeFileSync(path.join(__dirname, '../contract/plonk_vk.sol'), sc, {
+    flag: 'w',
+  });
+  console.log('Contract saved!');
+  process.exit();
 }
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
