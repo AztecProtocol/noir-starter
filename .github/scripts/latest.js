d17d6d055ae135a5fe0c6130b5ff0305fd9421ea
José Pedro Sousa <github@zepedro.me>
2023-12-20 16:18:05 +0000

    more JSON parsing

--- .github/scripts/latest.js
+++ .github/scripts/latest.js
@@ -16,11 +16,14 @@ async function main() {
     release => !release.tag_name.includes('aztec') && !release.tag_name.includes('nightly'),
   );
 
-  const latestStable = filtered.find(release => !release.prerelease).tag_name;
-  const latestPreRelease = filtered.find(release => release.prerelease).tag_name;
+  const latestStable = filtered.find(release => !release.prerelease).tag_name.substring(1);
+  const latestPreRelease = filtered.find(release => release.prerelease).tag_name.substring(1);
 
   // TODO: add the prerelease to this object!
-  const workflowOutput = JSON.stringify({ stable: latestStable, prerelease: latestPreRelease });
+  const workflowOutput = JSON.stringify({
+    stable: latestStable,
+    prerelease: latestPreRelease,
+  });
   console.log(workflowOutput); // DON'T REMOVE, GITHUB WILL CAPTURE THIS OUTPUT
 }
 
