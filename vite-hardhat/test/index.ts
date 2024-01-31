56fd4ee9e94056a84ecb944e3f3a6baf901bd275
José Pedro Sousa <outgoing@zpedro.dev>
2024-01-31 16:07:38 +0000

    moving to 0.23.0, fails with a bb target issue

--- vite-hardhat/test/index.ts
+++ vite-hardhat/test/index.ts
@@ -4,19 +4,30 @@ import hre from 'hardhat';
 import { Noir } from '@noir-lang/noir_js';
 import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
 
-import { compile, PathToFileSourceMap } from '@noir-lang/noir_wasm';
+import { compile, createFileManager } from '@noir-lang/noir_wasm';
 import { join } from 'path';
-import { ProofData } from '@noir-lang/types';
+import { CompiledCircuit, ProofData } from '@noir-lang/types';
 import { readFileSync } from 'fs';
 
-const getCircuit = async (name: string) => {
-  const sourcePath = new URL('../circuits/src/main.nr', import.meta.url);
-  const sourceMap = new PathToFileSourceMap();
+export async function getFile(file_path: string): Promise<ReadableStream<Uint8Array>> {
+  const file_url = new URL(file_path, import.meta.url);
+  const response = await fetch(file_url);
 
-  sourceMap.add_source_code(sourcePath.pathname, readFileSync(join(sourcePath.pathname), 'utf-8'));
-  const compiled = compile(sourcePath.pathname, undefined, undefined, sourceMap);
-  return compiled;
-};
+  if (!response.ok) throw new Error('Network response was not OK');
+
+  return response.body as ReadableStream<Uint8Array>;
+}
+
+async function getCircuit(name: string) {
+  const fm = createFileManager('/');
+  fm.writeFile('./src/main.nr', await getFile(`../circuits/src/main.nr`));
+  fm.writeFile('./Nargo.toml', await getFile(`../circuits/Nargo.toml`));
+  const result = await compile(fm);
+  if (!('program' in result)) {
+    throw new Error('Compilation failed');
+  }
+  return result.program as CompiledCircuit;
+}
 
 describe('It compiles noir program code, receiving circuit bytes and abi object.', () => {
   let noir: Noir;
