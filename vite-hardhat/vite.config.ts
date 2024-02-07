b92ff9237abb15c056deba09b8e1eb133df3a25b
ludamad <adam@aztecprotocol.com>
2024-02-07 19:39:04 +0000

    fix: top-level await bb.js 0.23

--- vite-hardhat/vite.config.ts
+++ vite-hardhat/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
   build: {
     target: 'esnext',
   },
