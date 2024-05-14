550e9da62a53df3e7e83edb97f48947efcf5397a
José Pedro Sousa <outgoing@zpedro.dev>
2024-05-14 17:27:51 +0100

    removing deprecated mumbai network

--- vite-hardhat/hardhat.config.cts
+++ vite-hardhat/hardhat.config.cts
@@ -40,13 +40,6 @@ const config: HardhatUserConfig = {
   },
 };
 
-if (process.env.MUMBAI_ALCHEMY_KEY && config.networks) {
-  config.networks.mumbai = {
-    url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.MUMBAI_ALCHEMY_KEY}`,
-    accounts: [process.env.MUMBAI_DEPLOYER_PRIVATE_KEY as string],
-  };
-}
-
 if (process.env.SEPOLIA_ALCHEMY_KEY && config.networks) {
   config.networks.sepolia = {
     url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.SEPOLIA_ALCHEMY_KEY}`,
--- vite-hardhat/utils/wagmi.tsx
+++ vite-hardhat/utils/wagmi.tsx
@@ -1,20 +1,14 @@
-import { createConfig, configureChains } from 'wagmi';
-import { localhost, polygonMumbai, sepolia } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { http, createConfig } from 'wagmi';
+import { localhost, sepolia } from 'wagmi/chains';
 import abi from './verifierAbi.json';
 import { chainId, verifier } from './addresses.json';
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [localhost, polygonMumbai, sepolia],
-  [publicProvider()],
-);
-
 export const config = createConfig({
-  autoConnect: true,
-  connectors: [new MetaMaskConnector({ chains })],
-  publicClient,
-  webSocketPublicClient,
+  chains: [localhost, sepolia],
+  transports: {
+    [localhost.id]: http(),
+    [sepolia.id]: http(),
+  },
 });
 
 export const contractCallConfig = {
