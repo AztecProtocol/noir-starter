a57decf3b32b00f0390913ce4f1fbd81e894f32b
José Pedro Sousa <outgoing@zpedro.dev>
2024-06-14 15:19:53 +0100

    hardhat supercharged pt2

--- vite-hardhat/test/test.sh
+++ vite-hardhat/test/test.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-NODE_OPTIONS='--experimental-loader ts-node/esm/transpile-only --no-warnings=ExperimentalWarning' hardhat test
+NODE_OPTIONS='--experimental-loader ts-node/esm/transpile-only --no-warnings=ExperimentalWarning' 
+
+NODE_OPTIONS=$NODE_OPTIONS hardhat compile
+NODE_OPTIONS=$NODE_OPTIONS hardhat test
